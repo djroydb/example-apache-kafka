38496439e23355ed0a5e0ab191a53a774dbbf98d
djroydb <djroydb@gmail.com>
2024-07-05 10:25:45 -0300

    strproducer First commit - Start project

---
+++ strproducer/src/main/java/br/com/robson/strproducer/StRproducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.robson.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StRproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StRproducerApplication.class, args);
+	}
+
+}
---
+++ strproducer/src/test/java/br/com/robson/strproducer/StRproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.robson.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StRproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
