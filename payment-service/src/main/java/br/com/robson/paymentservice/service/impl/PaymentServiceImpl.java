fd08cb9d86abb640a3c90e3f68a678ad78fec120
djroydb <djroydb@gmail.com>
2024-07-10 11:43:42 -0300

    Adicionado producer config

--- payment-service/src/main/java/br/com/robson/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/br/com/robson/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,13 +2,27 @@ package br.com.robson.paymentservice.service.impl;
 
 import br.com.robson.paymentservice.model.Payment;
 import br.com.robson.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o Pagamento {}", payment);
+        log.info("Recebi o Pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
